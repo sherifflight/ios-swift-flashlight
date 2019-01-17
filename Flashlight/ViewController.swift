60bbec9de8bfba36237e25bcf845d589e25e8e63
Eugene Dyakonov <eugene.dyakonov@gmail.com>
2019-01-18 03:58:52 +0400

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Евгений Дьяконов on 18/01/2019.
+//  Copyright © 2019 Евгений Дьяконов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
